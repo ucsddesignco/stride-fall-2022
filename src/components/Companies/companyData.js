e64fff40d3943243ad6e15af041ebb1377756797
baymarket96 <baymarkket96@gmail.com>
2022-11-01 13:22:56 -0700

    add company names and about

--- src/components/Companies/companyData.js
+++ src/components/Companies/companyData.js
@@ -1,7 +1,8 @@
 export const CompanyData = [
   {
-    name: "Sony",
-    about: "About text",
+    name: "Sony Electronics",
+    about:
+      "We are a global organization and seek individuals from a variety of generations, socioeconomic and educational backgrounds, and sexual orientations. Our employees are courageous, unique and diverse. They’re comfortable expressing their thoughts and opinions freely, and possess an unwavering passion and pride to deliver exceptional experiences to our customers.",
     hasOneToOne: true,
     hasInfoSession: true,
     isRecruiting: true,
@@ -9,8 +10,9 @@ export const CompanyData = [
     roles: ["role a", "role b"],
   },
   {
-    name: "Sony B",
-    about: "About text",
+    name: "Accenture Song",
+    about:
+      "We are a bold and generous group of creatives, designers, consultants, technologists, strategists and engineers working together to serve the modern customer and their ever-evolving needs. ",
     hasOneToOne: true,
     hasInfoSession: true,
     isRecruiting: true,
@@ -18,8 +20,9 @@ export const CompanyData = [
     roles: ["role a", "role b"],
   },
   {
-    name: "Sony C",
-    about: "About text",
+    name: "Deloitte Consulting LLP",
+    about:
+      'Deloitte is a $60-billion dollar professional services firm that constitutes one of the "Big Four" consulting and audit firms. In consulting, we have numerous Design Studio offerings, including the Dimension 10 Studio and the Deloitte Digital suite of studios (based in LA, SF, NY, Chicago, and Austin, and a dozen other cities). Our designers have a variety of backgrounds and we staff hundreds of end-to-end UXers, visual creatives, content designers, conversation designers, and UX researchers to help deliver innovative technology solutions for Fortune-500 clients in every industry (including technology, telecom, financial services, life sciences, hospitality, and energy). We build mobile apps, data dashboards, in-auto experiences, AR/VR solutions, industrial digital twins, and next generation conversational systems. Come join us on the bleeding edge of design for industry!',
     hasOneToOne: true,
     hasInfoSession: true,
     isRecruiting: true,
