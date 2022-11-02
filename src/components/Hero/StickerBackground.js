6b8ec5f2a5756c45b359ebd99a5b87764a87e816
baymarket96 <baymarkket96@gmail.com>
2022-11-01 17:15:21 -0700

    added default stickers

--- src/components/Hero/StickerBackground.js
+++ src/components/Hero/StickerBackground.js
@@ -4,6 +4,7 @@ import { Sticker } from "./Sticker";
 import { useStickerBackground } from "./useStickerBackground";
 import styles from "./styles.module.scss";
 import * as stickerImages from "../../images/stickers";
+import { random } from "../../utils/utils";
 
 const StickerBackground = () => {
   const stickerBackground = useStickerBackground();
@@ -22,12 +23,30 @@ const StickerBackground = () => {
       onMouseDown={stickerBackground.handleMouseDown}
       onMouseLeave={stickerBackground.handleMouseLeave}
     >
-      {/* <Sticker
-        x={0.5}
-        y={0.5}
-        isVisible={sticker.isVisible}
-        imageURL={stickerImages.images[sticker.number]}
-      /> */}
+      <Sticker
+        x={0.9}
+        y={0.6}
+        isVisible={true}
+        imageURL={stickerImages.images[1]}
+      />
+      <Sticker
+        x={0.1}
+        y={0.2}
+        isVisible={true}
+        imageURL={stickerImages.images[5]}
+      />
+      <Sticker
+        x={0.8}
+        y={0.1}
+        isVisible={true}
+        imageURL={stickerImages.images[7]}
+      />
+      <Sticker
+        x={0.2}
+        y={0.8}
+        isVisible={true}
+        imageURL={stickerImages.images[2]}
+      />
       {stickerBackground.stickers.map((sticker, index) => (
         <Sticker
           x={sticker.x}
