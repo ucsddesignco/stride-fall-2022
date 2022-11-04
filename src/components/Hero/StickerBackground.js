3082f335154dca8e5b79bcd7d2062642749cf8c1
baymarket96 <baymarkket96@gmail.com>
2022-11-04 13:43:51 -0700

    randomized stickers

--- src/components/Hero/StickerBackground.js
+++ src/components/Hero/StickerBackground.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { json } from "react-router-dom";
 import { Sticker } from "./Sticker";
 import { useStickerBackground } from "./useStickerBackground";
@@ -8,7 +8,7 @@ import { random } from "../../utils/utils";
 
 const StickerBackground = () => {
   const stickerBackground = useStickerBackground();
-
+  const [reversedStickers] = useState(Math.random() < 0.5 ? true : false);
   useEffect(() => {
     stickerImages.images.map((image) => {
       new Image().src = image;
@@ -24,31 +24,31 @@ const StickerBackground = () => {
       onMouseLeave={stickerBackground.handleMouseLeave}
     >
       <Sticker
-        x={0.95}
+        x={Math.abs((reversedStickers ? 0 : -1) + 0.95)}
         y={0.8}
         isVisible={true}
         imageURL={stickerImages.images[1]}
       />
       <Sticker
-        x={0.05}
+        x={Math.abs((reversedStickers ? 0 : -1) + 0.05)}
         y={0.1}
         isVisible={true}
         imageURL={stickerImages.images[5]}
       />
       <Sticker
-        x={0.8}
+        x={Math.abs((reversedStickers ? 0 : -1) + 0.8)}
         y={0.15}
         isVisible={true}
         imageURL={stickerImages.images[7]}
       />
       <Sticker
-        x={0.1}
+        x={Math.abs((reversedStickers ? 0 : -1) + 0.1)}
         y={0.85}
         isVisible={true}
         imageURL={stickerImages.images[2]}
       />
       <Sticker
-        x={0.6}
+        x={Math.abs((reversedStickers ? 0 : -1) + 0.6)}
         y={0.95}
         isVisible={true}
         imageURL={stickerImages.images[12]}
